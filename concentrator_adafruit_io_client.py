ac7c0a475c414bafe0103d78efd843a49ef3678f
Liang C <liangc@ospray.bluesky>
2018-03-04 10:43:14 -0500

    handle exceptions from adafruit/io

--- concentrator_adafruit_io_client.py
+++ concentrator_adafruit_io_client.py
@@ -51,26 +51,31 @@ hub_serial = serial.Serial('/dev/ttyACM2', 115200)
 send_to_adafruit = True
 
 while True:
-    line = hub_serial.readline()
-    sys.stdout.write('line: ' + line)
-    data = parse_line(hub_info_pattern, line)
+    try:
+        line = hub_serial.readline()
+        sys.stdout.write('line: ' + line)
+        data = parse_line(hub_info_pattern, line)
+        if len(data) != 7:
+            raise ValueError('Oops, invalid input from concentrator, # of data was %d!' % len(data))
+        
+        sys.stdout.write('rssi = %d, '% int(data[1]))
+        sys.stdout.write('cpu_temp = %.2f, ' % (int(data[2])/1.0))
+        sys.stdout.write('battery_volt = %.2f, ' % (int(data[3])/256.0)) 
+        sys.stdout.write('temp = %.2f, ' % (int(data[4])/100.0))
+        sys.stdout.write('pressure = %.4f, ' % (int(data[5])/10000.0))
+        sys.stdout.write('humidity = %.2f' % (int(data[6])/1000.0))
+        sys.stdout.write('\n')
     
-    sys.stdout.write('rssi = %d, '% int(data[1]))
-    sys.stdout.write('cpu_temp = %.2f, ' % (int(data[2])/1.0))
-    sys.stdout.write('battery_volt = %.2f, ' % (int(data[3])/256.0)) 
-    sys.stdout.write('temp = %.2f, ' % (int(data[4])/100.0))
-    sys.stdout.write('pressure = %.4f, ' % (int(data[5])/10000.0))
-    sys.stdout.write('humidity = %.2f' % (int(data[6])/1000.0))
-    sys.stdout.write('\n')
-
-    if send_to_adafruit:
-        aio.send('rssi', int(data[1]))
-        aio.send('cpu_temp', int(data[2])/1.0)
-        aio.send('battery_volt', int(data[3])/256.0) 
-        aio.send('temp', int(data[4])/100.0)
-        aio.send('pressure', int(data[5])/10000.0)
-        aio.send('humidity', int(data[6])/1000.0)
-
+        if send_to_adafruit:
+            aio.send('rssi', int(data[1]))
+            aio.send('cpu_temp', int(data[2])/1.0)
+            aio.send('battery_volt', int(data[3])/256.0) 
+            aio.send('temp', int(data[4])/100.0)
+            aio.send('pressure', int(data[5])/10000.0)
+            aio.send('humidity', int(data[6])/1000.0)
+    except Exception, e:
+        sys.stdout.write('Oops, exception: ')
+        print e
 # # Send a value to the feed 'Test'.  This will create the feed if it doesn't
 # # exist already.
 # aio.send('Test', 42)
